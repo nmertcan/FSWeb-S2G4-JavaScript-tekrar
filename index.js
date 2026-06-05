798f5fdce4e415a6a001d4d0c4d1fe15b5145c9d
Mert <nmertcan@icloud.com>
2026-06-06 00:46:33 +0300

    Karenin Alanı Sorusu Çözüldü

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ function KareninAlani(kenaruzunlugu) {
   return kenaruzunlugu * kenaruzunlugu;
 }
 
+console.log(KareninAlani(10));
+
 /* (Oto test yok) Yukarıdaki KareninAlani fonksiyonunu kenar uzunluğu = 10 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
 /* GÖREV 1:  
